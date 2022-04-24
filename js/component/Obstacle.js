2289ab18b12c64d0d3da821eddaaef7c99e2b6f0
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2022-04-24 19:17:25 +0900

    具象クラスに処理をうつす

--- js/component/Obstacle.js
+++ js/component/Obstacle.js
@@ -8,16 +8,23 @@ export class Obstacle extends Ball {
      * コンストラクタ
      *
      */
-    constructor() {
+    constructor(canvas, context, sum_count, integer) {
         super();
+        this.canvas = canvas;
+        this.context = context;
         this.color = "rgb(0, 0, 0)";
+        this.x = integer * this.canvas.width / this.counts + (this.canvas.width / sum_count / 2);
+        this.y = Math.random() * (0 + this.canvas.height) - this.canvas.height; // 隕石のy座標
+        // obstacle.y = Math.random() * (0 + this.canvas.height) - this.canvas.height; // 隕石のy座標
+        this.radius = Math.random() * (this.canvas.width / sum_count / 2 - 10) + 10;   // 隕石の半径
+        this.speed = Math.random() * (15 - 1) + 1;                                  // 隕石の速さ
         // this.counts = 5;
         // for(var i = 0; i < this.counts; i++) {
         //     const obstacle = new Ball();
-        //     obstacle.x = i * canvas.area.width / this.counts + (canvas.area.width / this.counts / 2);
+        //     obstacle.x = i * this.canvas.area.width / this.counts + (this.canvas.area.width / this.counts / 2);
         //     obstacle.color = "rgb(0, 0, 0)";
-        //     obstacle.y = Math.random() * (0 + canvas.area.height) - canvas.area.height; // 隕石のy座標
-        //     obstacle.radius = Math.random() * (canvas.area.width / this.counts / 2 - 10) + 10;   // 隕石の半径
+        //     obstacle.y = Math.random() * (0 + this.canvas.area.height) - this.canvas.area.height; // 隕石のy座標
+        //     obstacle.radius = Math.random() * (this.canvas.area.width / this.counts / 2 - 10) + 10;   // 隕石の半径
         //     obstacle.sp = Math.random() * (15 - 1) + 1;                                  // 隕石の速さ
         //     this.obstacles.push(obstacle);
         // }
@@ -29,20 +36,20 @@ export class Obstacle extends Ball {
     init() {
     }
 
-    draw(player, canvas, context) {
+    draw(player) {
         this.y += this.speed;              // それぞれのスピードで動かす
         if (Obstacle.collision(player, this) === true) {  // 衝突判定結果がtrueなら
             // window.clearInterval(this.timer);            // タイマーを止める
             alert("GAME OVER");
         }
-        if (this.y > canvas.height + this.radius) {  // もし隕石が画面から消えたら
+        if (this.y > this.canvas.height + this.radius) {  // もし隕石が画面から消えたら
             // randomizeMeteo(this);           // 位置やサイズを初期化・ランダム化する
         }
-        context.beginPath();                    // 描画開始
-        context.arc(this.x, this.y, this.radius,  // 円を描く arc(x, y, 半径, 開始角度, 終了角度)
+        this.context.beginPath();                    // 描画開始
+        this.context.arc(this.x, this.y, this.radius,  // 円を描く arc(x, y, 半径, 開始角度, 終了角度)
             0, 2 * Math.PI);            // 角度の単位はラジアン（2π = 360度）で指定
-        context.fillStyle = this.color      // 塗りつぶす色の設定
-        context.fill();                         // 塗る
+        this.context.fillStyle = this.color      // 塗りつぶす色の設定
+        this.context.fill();                         // 塗る
     }
 
 
--- js/function/Draw.js
+++ js/function/Draw.js
@@ -11,21 +11,20 @@ export class Draw {
      * コンストラクタ
      */
     constructor() {
-
         this.canvas = document.getElementById('canvas');
         this.context = this.canvas.getContext('2d');
 
-        this.player = new Player();
+        this.player = new Player(this.canvas, this.context);
 
         this.obstacles = [];
         this.counts = 5;
         for (var i = 0; i < this.counts; i++) {
-            const obstacle = new Obstacle();
-            obstacle.x = i * this.canvas.width / this.counts + (this.canvas.width / this.counts / 2);
-            obstacle.y = Math.random() * (0 + this.canvas.height) - this.canvas.height; // 隕石のy座標
-            obstacle.radius = Math.random() * (this.canvas.width / this.counts / 2 - 10) + 10;   // 隕石の半径
-            obstacle.speed = Math.random() * (15 - 1) + 1;                                  // 隕石の速さ
-            this.obstacles.push(obstacle);
+            // const obstacle = new Obstacle(this.canvas, this.context, this.counts, i);
+            // obstacle.x = i * this.canvas.width / this.counts + (this.canvas.width / this.counts / 2);
+            // // obstacle.y = Math.random() * (0 + this.canvas.height) - this.canvas.height; // 隕石のy座標
+            // obstacle.radius = Math.random() * (this.canvas.width / this.counts / 2 - 10) + 10;   // 隕石の半径
+            // obstacle.speed = Math.random() * (15 - 1) + 1;                                  // 隕石の速さ
+            this.obstacles.push(new Obstacle(this.canvas, this.context, this.counts, i));
         }
 
         this.deviceMotion = new DeviceMotion();
@@ -34,9 +33,9 @@ export class Draw {
 
         this.timer = window.setInterval(() => {
             this.text.draw(this.deviceMotion);
-            this.player.draw(this.deviceMotion, this.canvas, this.context);         // ★drawBall 関数を実行
+            this.player.draw(this.deviceMotion);         // ★drawBall 関数を実行
             for (var i = 0; i < this.obstacles.length; i++) {
-                this.obstacle[i].draw(this.player, this.canvas, this.context);
+                this.obstacle[i].draw(this.player);
             }
         }, 50);
     }
