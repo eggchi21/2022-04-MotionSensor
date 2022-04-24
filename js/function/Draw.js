7e0693adcdb2d06767b61811061d995023eedd41
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2022-04-24 19:39:48 +0900

    Update Draw.js

--- js/function/Draw.js
+++ js/function/Draw.js
@@ -19,7 +19,7 @@ export class Draw {
         this.obstacles = [];
         this.counts = 5;
         for (var i = 0; i < this.counts; i++) {
-            const obstacle = new Obstacle(this.canvas, this.context, this.counts, i);
+            // const obstacle = new Obstacle(this.canvas, this.context, this.counts, i);
             // obstacle.x = i * this.canvas.width / this.counts + (this.canvas.width / this.counts / 2);
             // // obstacle.y = Math.random() * (0 + this.canvas.height) - this.canvas.height; // 隕石のy座標
             // obstacle.radius = Math.random() * (this.canvas.width / this.counts / 2 - 10) + 10;   // 隕石の半径
@@ -35,6 +35,8 @@ export class Draw {
             this.text.draw(this.deviceMotion);
             this.player.draw(this.deviceMotion);         // ★drawBall 関数を実行
             for (var i = 0; i < this.obstacles.length; i++) {
+            console.log('timer', this.obstacles[i]);
+
                 this.obstacles[i].draw(this.player);
             }
         }, 110);
