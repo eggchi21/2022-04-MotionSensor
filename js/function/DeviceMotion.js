79ab08b4897d657407a4273611b4d7e3ff83dd87
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2022-04-24 23:42:32 +0900

    Update DeviceMotion.js

--- js/function/DeviceMotion.js
+++ js/function/DeviceMotion.js
@@ -8,10 +8,10 @@ export class DeviceMotion {
     constructor() {
         alert('ok');
         // ボタンクリックでrequestDeviceMotionPermission実行
-        const startButton = document.getElementById("start-button")
-        startButton.addEventListener('click', this.requestDeviceMotionPermission, false)
+        // const startButton = document.getElementById("start-button")
+        // startButton.addEventListener('click', this.requestDeviceMotionPermission, false)
         // this.requestDeviceMotionPermission();
-        // window.onload = () => { this.requestDeviceMotionPermission() }
+        window.onload = () => { this.requestDeviceMotionPermission }
         this.aX = 0;
         this.aY = 0;
         this.coefficient = ["iPhone", "iPad", "iPod"].includes(navigator.platform) ? -1 : 1;
